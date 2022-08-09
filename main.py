eabff6d71c63eadf35f0d6bd7bde64e3f4ce3a2a
Lukas Petrauskas <lukas_petrauskas@yahoo.com>
2022-08-09 18:47:58 +0300

    Added inputs and multiply action

---
+++ main.py
@@ -0,0 +1,7 @@
+first_number = float(input("input first number: "))
+second_number = float(input("input second number: "))
+what_to_do = (input("'*'"))
+if what_to_do == '*':
+    print(f"{first_number} * {second_number} =", first_number * second_number)
+else:
+    print("Whatafak you want from me")
