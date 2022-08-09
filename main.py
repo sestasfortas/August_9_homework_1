868e655c9d47af243af88467657caa0e532e7757
Lukas Petrauskas <lukas_petrauskas@yahoo.com>
2022-08-09 18:50:05 +0300

    Added divison action

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 first_number = float(input("input first number: "))
 second_number = float(input("input second number: "))
-what_to_do = (input("'*'"))
+what_to_do = (input("'*','/'"))
 if what_to_do == '*':
     print(f"{first_number} * {second_number} =", first_number * second_number)
+elif what_to_do == '/':
+    print(f'{first_number} / {second_number} =', first_number / second_number)
 else:
     print("Whatafak you want from me")
